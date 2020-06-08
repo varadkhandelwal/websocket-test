1b0a742626bfad9c0db863136cd219107ed4dee0
varadkhandelwal <simplyvarad@gmail.com>
2020-06-07 20:19:41 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+    console.log('A new WebSocket connection has been established');
+});
+
+setInterval(function() {
+  var stockprice = Math.floor(Math.random() * 1000);
+  io.emit('stock price update', stockprice);
+}, 50);
+
+http.listen(8000, function() {
+    console.log('Listening on *:8000');
+});
